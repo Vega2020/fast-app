0433e5e86486751533ea501d94bef4ed29da637c
Jeremy Flowers <flowersje08@gmail.com>
2020-11-06 16:01:08 -0700

    initial setup, changed header

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is going to be the FAST app.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://scholar.google.com/scholar_url?url=http://www.academia.edu/download/43254871/RELIABILITY_AND_VALIDITY_OF_THE_FUNCTION20160301-17957-yksq2t.pdf&hl=en&sa=X&ei=tdOlX8TzFsS9yQSqh5jIAQ&scisig=AAGBfm157K3aEe6pyssgQLmlvnjzeHNX0w&nossl=1&oi=scholarr"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn about the Functional Assessment Screening Tool
         </a>
       </header>
     </div>
